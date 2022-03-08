a372ce4b24c6dcdc4109831276c47a359d7d8f2d
Michael Bierman <mbierman@users.noreply.github.com>
2022-03-08 14:54:00 -0800

    Create unifi-uninstall.sh

---
+++ unifi-uninstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "WARNING! this will uninstall unifi from your Firewalla and remove all settings files.\n\nThere is no way to recover any lost files. Do not do this unless you are sure! "
+
+read -p "Please press 'Y' to continue or any key to stop: " -n1 now
+
+if ! [[ "$now" = "Y" || "$now" = "y" ]]; then
+        exit
+fi
+
+cd ~/.firewalla/run/docker/unifi
+sudo docker container stop unifi && sudo docker container rm unifi
+sudo docker system prune -a
+sudo docker image prune -a
+sudo docker volume prune
+sudo docker ps
+rm /home/pi/.firewalla/config/post_main.d/start_unifi.sh
+rm ~/.firewalla/config/dnsmasq_local/unifi
+rm -rf /home/pi/.firewalla/run/docker/unifi
+sudo rm -rf /data/unifi
