37554ff2f76c01f9ed837aef773a4c4c591fa922
Michael Bierman <mbierman@users.noreply.github.com>
2023-08-12 18:40:08 -0700

    Update unifi-uninstall.sh

--- unifi-uninstall.sh
+++ unifi-uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v 2.0
+# v 2.1
 
 echo -e "WARNING! this will uninstall unifi from your Firewalla and remove all settings files.\n\nThere is no way to recover any lost files. Do not do this unless you are sure! "
 
@@ -23,5 +23,12 @@ sudo rm -rf /home/pi/.firewalla/run/docker/unifi  2> /dev/null
 sudo rm -rf /data/unifi  2> /dev/null
 sudo ip route del 172.17.0.0/16 2> /dev/null
 sudo systemctl restart firerouter_dns
-sudo rm -rf /data/unifi-uninstall.sh
+
+read -p "Please press 'Y' to continue or any key to stop: " -n1 now
+
+if  [[ "$now" = "Y" || "$now" = "y" ]]; then
+        sudo rm -rf /data/unifi-uninstall.sh && echo -e "\nUninstall script removed.\n\n"
+        exit
+fi
+
 echo -e  "\n\nfin."
