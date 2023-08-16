b3d06f01d12b64526642fc7f775b760e61a14ace
Michael Bierman <mbierman@users.noreply.github.com>
2023-08-16 06:56:20 -0700

    Update unifi_docker_install.sh

--- unifi_docker_install.sh
+++ unifi_docker_install.sh
@@ -53,7 +53,7 @@ while true; do
     if ping -W 1 -c 1 172.16.1.2 > /dev/null 2>&1 && ip route show table lan_routable | grep -q '172.16.1.0'; then
         break
     fi
-    
+    echo $ID
     sudo ip route add 172.16.1.0/24 dev br-$ID table lan_routable
     sudo ip route add 172.16.1.0/24 dev br-$ID table wan_routable
 
