e2819274a72c5da924bccb7991ce19db38b008b5
Michael Bierman <mbierman@users.noreply.github.com>
2024-04-20 11:34:31 -0700

    Update unifi-uninstall.sh

--- unifi-uninstall.sh
+++ unifi-uninstall.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 # v 2.3
 
+# Countdown function
+countdown() {
+    seconds=$1
+    while [ $seconds -gt 0 ]; do
+        echo -ne "Press Ctrl+C to stop execution. Continuing in $seconds seconds...\r"
+        sleep 1
+        : $((seconds--))
+    done
+    echo ""
+}
+
+# Display warning message
+echo -e "WARNING! This will uninstall unifi from your Firewalla and remove all settings files.\n\nThere is no way to recover any lost files. Do not proceed unless you are sure!\n"
+
+# Perform countdown for 10 seconds
+countdown 10
+
 echo -e "\n\nStarting uninstall...\n"
 sudo docker container stop unifi && sudo docker container rm unifi
 sudo docker network rm unifi_default
