2c30769439fd11f491a1e5e37ed555ad7940f0e1
Michael Bierman <mbierman@users.noreply.github.com>
2024-04-20 11:18:43 -0700

    Update unifi-uninstall.sh

--- unifi-uninstall.sh
+++ unifi-uninstall.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
-# v 2.2
+# v 2.3
 
+# Function to display script usage
+usage() {
+    echo "Usage: $0 [-y]"
+    echo "  -y  Automatically answer 'yes' to all prompts"
+    exit 1
+}
+
+# Default value for automatic 'yes' answer
+automatic_yes=false
+
+# Parse command-line options
+while getopts ":y" opt; do
+    case ${opt} in
+        y )
+            automatic_yes=true
+            ;;
+        \? )
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Display warning message
 echo -e "WARNING! this will uninstall unifi from your Firewalla and remove all settings files.\n\nThere is no way to recover any lost files. Do not do this unless you are sure! "
 
-# Providing 'Y' as input to the first prompt
-now="Y"
-
+# Prompt user to continue unless automatic 'yes' answer is enabled
+if [ "$automatic_yes" = false ]; then
+    read -p "Please press 'Y' to continue or any key to stop: " -n1 now
+    echo ""
+    if ! [[ "$now" = "Y" || "$now" = "y" ]]; then
+        echo -e "\nNo changes made.\n\n"
+        exit 0
+    fi
+fi
 
 echo -e "\n\nStarting uninstall...\n"
 sudo docker container stop unifi && sudo docker container rm unifi
@@ -25,11 +55,12 @@ sudo ip route del 172.17.0.0/16 2> /dev/null
 sudo systemctl restart firerouter_dns
 
 echo -e "\n\n"
-read -p "Do you want to remove the uninstall script? Please press 'Y' to continue or any key to stop: " -n1 now
-
-if  [[ "$now" = "Y" || "$now" = "y" ]]; then
+if [ "$automatic_yes" = false ]; then
+    read -p "Do you want to remove the uninstall script? Please press 'Y' to continue or any key to stop: " -n1 now
+    echo ""
+    if  [[ "$now" = "Y" || "$now" = "y" ]]; then
         sudo rm -rf /data/unifi-uninstall.sh && echo -e "\nUninstall script removed.\n\n"
-        exit
+    fi
 fi
 
 echo -e  "\n\nfin."
