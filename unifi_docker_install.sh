16192c3b98dec2e0d5395e1fbbb45724737acf48
Michael Bierman <mbierman@users.noreply.github.com>
2024-11-10 12:09:41 -0800

    Update unifi_docker_install.sh

--- unifi_docker_install.sh
+++ unifi_docker_install.sh
@@ -4,7 +4,7 @@
 
 # This is used later but get the information up front. 
 
-[ -f /etc/update-motd.d/00-header ] && series=$(/etc/update-motd.d/00-header | grep "Welcome to" | sed -e "s|Welcome to ||g" -e "s|FIREWALLA ||g" -e "s|\s[0-9].*$||g") || series="" ; echo "..."
+[ -f /etc/update-motd.d/00-header ] && series=$(/etc/update-motd.d/00-header | grep "Welcome to" | sed -e "s|Welcome to ||g" -e "s|FIREWALLA ||g" -e "s|\s[0-9].*$||g") || series=""
 
 if [[ "$series" == *"gold-se"* ]]; then
     echo "Gold SE..."
